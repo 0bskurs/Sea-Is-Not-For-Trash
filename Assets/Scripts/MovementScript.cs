7d4290e9bd5d8ba3e44afac31f6b8c03270b07bc
Obskur <117692835+0bskurs@users.noreply.github.com>
2025-08-08 11:31:21 -0300

    3

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
+    private Vector2 currentPosition;
+    public bool keyIsPressed;
+    public float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
