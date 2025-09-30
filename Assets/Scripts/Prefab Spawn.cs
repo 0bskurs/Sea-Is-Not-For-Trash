e94c26389fe9e504bc616c72e6ff590e411fd60a
Obskur <magnoart16@gmail.com>
2025-09-29 22:31:44 -0300

    test for new prefab generation

--- Assets/Scripts/Prefab Spawn.cs
+++ Assets/Scripts/Prefab Spawn.cs
@@ -12,12 +12,38 @@ using UnityEngine.SceneManagement;
 public class PrefabSpawnNew : MonoBehaviour
 {
     [System.Serializable]
-    public class PrefabOrders
+    public class Presets
     {
-        public int callPrefabs;
-        
+        public string preset;
+        public List<bool> hasGameObject;
+        public List<int> whichGameObject;
+
     }
-    public List<int> PrefabsOrder;
+
+    [System.Serializable]
+    public class PrefabVariation
+    {
+        public string objectName;
+        public int objectNumber;
+        public GameObject prefabVariant;
+    }
+    public List<Presets> presets_;
+    public List<PrefabVariation> prefabVariations;
+
+    public void Start()
+    {
+        for (int i = 0; i < presets_[0].hasGameObject.Count; i++)
+        {
+            if (presets_[0].hasGameObject[i] == true)
+            {
+                Debug.Log($"{i} " + "IsTrue!");
+            }
+            else
+            {
+                Debug.Log($"{i} "+"IsFalse!");
+            }
+        }
+    }
     // 1 : Garrafa
     // 2 : Radioativo
     // 3
