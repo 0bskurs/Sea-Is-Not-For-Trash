b694eb81d9214732a8b05e8639fff71a7faea4f8
Obskur <magnoart16@gmail.com>
2025-10-12 19:23:12 -0300

    ???

--- Assets/Scripts/Player(feito por felipe)/LifeSystem.cs
+++ Assets/Scripts/Player(feito por felipe)/LifeSystem.cs
@@ -5,106 +5,58 @@ using UnityEngine.Animations;
 using Unity.VisualScripting;
 using System.Collections;
 using UnityEngine.SceneManagement;
-using System.Collections.Generic;
-using UnityEngine.UI;
 public class LifeSystem : MonoBehaviour
 {
     [SerializeField] private int MaxLife = 3;
     [SerializeField] private int Currentlife;
-    [SerializeField] private List<Image> _healthSprites;
-    //[SerializeField] private GetAnimatorVida getAnimatorVida;
-    //[SerializeField] private int vida;
-
-    [SerializeField] private int whichHeartEnableDisable;
+    [SerializeField] private GetAnimatorVida getAnimatorVida;
+    [SerializeField] private int vida;
+    
+    
     [Header("How much until other game object is destroyed (in milliseconds).")][SerializeField] private int waitForTask;
 
     
     private void Start()
     {
         Currentlife = MaxLife;
-        whichHeartEnableDisable = MaxLife - 1;
+
 
         
-        //vida = MaxLife;
-        //if (getAnimatorVida.animator = null)
-        //{
-        //    Debug.LogWarning("Faltando Animator!");
-        //}
+        vida = MaxLife;
+        if (getAnimatorVida.animator = null)
+        {
+            Debug.LogWarning("Faltando Animator!");
+        }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Trash"))
         {
            
-            Debug.Log("Em contato com lixo");
+            Debug.Log("Em contato");
             Invoke("DisableImages", 0.1f);
             Destroy(other.gameObject);
             
         }
-        if (other.CompareTag("Heal"))
-        {
-
-            Debug.Log("Em contato com cura");
-            Invoke("EnableImages", 0.1f);
-            Destroy(other.gameObject);
-
-        }
     }
-
-
+    
+    
     async Task DisableImages()
     {
-        Physics2D.IgnoreLayerCollision(6, 7, true);
-
-        if (whichHeartEnableDisable != -1)
-        {
-            _healthSprites[whichHeartEnableDisable].enabled = false;
-        }
+        await Task.Delay(500);
         Currentlife -= 1;
-        //vida = Currentlife;
-        whichHeartEnableDisable -= 1;
+        vida = Currentlife;
+        getAnimatorVida.animator.SetFloat("Vida", vida);
 
-        //getAnimatorVida.animator.SetFloat("Vida", vida);
-
-        if (Currentlife <= 0)
+        if (Currentlife <= 0) 
         {
-            Physics2D.IgnoreLayerCollision(6, 7, false);
+            
             await Task.Delay(2000);
             SceneManager.LoadSceneAsync(2);
         } // Subtituir por uma troca de cena,SceneManagement
-        await Task.Delay(3000);
-        Physics2D.IgnoreLayerCollision(6, 7, false);
+
 
     }
-    private void OnDisable()
-    {
-        Physics2D.IgnoreLayerCollision(6, 7, false);
-    }
-    async Task EnableImages()
-    {
-        if (Currentlife < MaxLife && Currentlife > 0)
-        {
-            Physics2D.IgnoreLayerCollision(6, 7, true);
-            Currentlife += 1;
-            whichHeartEnableDisable += 1;
-            await Task.Delay(250);
-            _healthSprites[whichHeartEnableDisable].enabled = true;
-            await Task.Delay(1250);
-            Physics2D.IgnoreLayerCollision(6, 7, false);
+    
 
-        }
-        //Currentlife += 1;
-        //if (whichHeartEnableDisable != -1 && Currentlife < MaxLife)
-        //{
-        //    _healthSprites[whichHeartEnableDisable].enabled = true;
-        //}
-        //if (Currentlife > MaxLife)
-        //{
-        //    Currentlife = MaxLife;
-        //    Physics2D.IgnoreLayerCollision(6, 7, false);
-        //}
-        //vida = Currentlife;
-        //getAnimatorVida.animator.SetFloat("Vida", vida);
-    }
-
-    }
+}
